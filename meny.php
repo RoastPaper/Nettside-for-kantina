18cee00035e52538531dc47592217f2f7bd78556
RoastPaper <william.haaje@gmail.com>
2025-04-04 10:23:58 +0000

    Trengte ikke her heller.

--- meny.php
+++ meny.php
@@ -5,12 +5,6 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header('Location: index.html');
     exit;
 }
-require_once('/var/www/william_domain/mysql.php');
-
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 ?>
 <!DOCTYPE html>
 <html lang="no">
