d4ebd7815c7005e1178da4382ed14122a9f8e561
RoastPaper <william.haaje@gmail.com>
2025-04-04 09:11:41 +0000

    Dette gjør at det er ikke mulig flere bbrukernavn i nettsiden ved å sjekke databasen om det får en verdi.

--- register.php
+++ register.php
@@ -15,11 +15,19 @@ if (mysqli_connect_errno()) {
     exit('Det er noe feil med tilkoblingen til MySQl: ' . mysqli_connect_error());
 }
 
- if ($stmt = $con->prepare('INSERT INTO Account (username, password, email, date) VALUES (?, ?, ?, ?)')) {
-    $stmt->bind_param('ssss', $username, $password, $email, $date);
+if ($stmt = $con->prepare('SELECT id, password FROM Account WHERE username = ?')) {
+    $stmt->bind_param('s', $username);
     $stmt->execute();
-    $stmt->close();
- }
+    $stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        echo 'Brukernavnet finnes allerede';
+    } else {
+        $stmt = $con->prepare('INSERT INTO Account (username, password, email, date) VALUES (?, ?, ?, ?)');
+        $stmt->bind_param('ssss', $username, $password, $email, $date);
+        $stmt->execute();
+        $stmt->close();
+    }
+}
 $con->close();
 echo 'Du har registert deg, du kan klikke denne linken for å logge deg inn! <a href="login.html">Klikk her</a>';
 ?>
