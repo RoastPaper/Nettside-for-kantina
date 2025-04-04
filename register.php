a4c1ef3f8f5b0e0a2ba3d9672cba72ab85b99926
RoastPaper <william.haaje@gmail.com>
2025-04-04 09:03:12 +0000

    Jeg har nå lagd en enkel registering php fil.

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+require_once('/var/www/william_domain/mysql.php');
+
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
+$email = trim($_POST['email']);
+
+$day = intval($_POST['day']);
+$month = intval($_POST['month']);
+$year = intval($_POST['year']);
+$date = sprintf("%04d-$02d-%02d", $year, $month, $day);
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    exit('Det er noe feil med tilkoblingen til MySQl: ' . mysqli_connect_error());
+}
+
+ if ($stmt = $con->prepare('INSERT INTO Account (username, password, email, date) VALUES (?, ?, ?, ?)')) {
+    $stmt->bind_param('ssss', $username, $password, $email, $date);
+    $stmt->execute();
+    $stmt->close();
+ }
+$con->close();
+echo 'Du har registert deg, du kan klikke denne linken for å logge deg inn! <a href="login.html">Klikk her</a>';
+?>
